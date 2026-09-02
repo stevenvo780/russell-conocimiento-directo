e6981d28fd4db56ad148b003d08ededb31ee7420
stevenvo780 <stevenvallejo780@gmail.com>
2026-09-02 01:54:30 +0000

    fix: restore GitHub Pages base path

---
+++ assets/Capitulo5-C2k-LUvW.js
@@ -0,0 +1,2 @@
+var e=`/russell-conocimiento-directo/assets/Capitulo5-CIPHSvSw.md`;export{e as t};
+//# sourceMappingURL=Capitulo5-C2k-LUvW.js.map
--- assets/Capitulo5-DnWFU8K0.js
+++
@@ -1,2 +0,0 @@
-var e=`/assets/Capitulo5-CIPHSvSw.md`;export{e as t};
-//# sourceMappingURL=Capitulo5-DnWFU8K0.js.map
